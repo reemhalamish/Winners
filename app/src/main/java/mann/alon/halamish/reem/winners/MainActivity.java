84dd0e6d0921b23fef13f70d06cfa8f039dadc78
reemhalamish <reem.halamish@gmail.com>
2016-05-27 11:42:15 +0300

    first version

---
+++ app/src/main/java/mann/alon/halamish/reem/winners/MainActivity.java
@@ -0,0 +1,29 @@
+package mann.alon.halamish.reem.winners;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button divine_score = (Button) findViewById(R.id.button);
+        divine_score.setText("התוצאה תהיה:");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
